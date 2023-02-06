d75fb3c51fd2097e951e786270310f043143e655
Seonho Kim <sunhowold@naver.com>
2023-02-07 01:04:15 +0900

    엔티티 매핑 - 필드와 컬럼 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,7 +1,16 @@
 package hellojpa;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
 @Entity
 public class Member {
@@ -9,7 +18,28 @@ public class Member {
 	@Id
 	private Long id;
 
+	@Column(name = "name", updatable = false)
 	private String name;
+
+	private Integer age;
+
+	@Enumerated(EnumType.STRING)
+	private RoleType roleType;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date lastModifiedDate;
+
+	@Lob
+	private String description;
+
+	@Transient
+	private int temp;
+
+	public Member() {
+	}
 
 	public Long getId() {
 		return id;
