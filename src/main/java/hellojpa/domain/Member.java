80e8e4e4dc4067a5a5f2d03d43a750c7463cbfac
Seonho Kim <sunhowold@naver.com>
2023-02-08 18:48:16 +0900

    다양한 연관관계 매핑: 일대일 - 주 테이블 양방향 매핑

--- src/main/java/hellojpa/domain/Member.java
+++ src/main/java/hellojpa/domain/Member.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Member {
@@ -18,12 +19,13 @@ public class Member {
 	@Column(name = "USERNAME")
 	private String username;
 
-	// @Column(name = "TEAM_ID")
-	// private Long teamId;
-
 	@ManyToOne
 	@JoinColumn(name = "TEAM_ID")
 	private Team team;
+
+	@OneToOne
+	@JoinColumn(name = "LOCKER_ID", unique = true)
+	private Locker locker;
 
 	public Long getId() {
 		return id;
