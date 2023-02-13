15c307405a81f0788a62dbea846a95033393b4b7
Seonho Kim <sunhowold@naver.com>
2023-02-14 01:07:47 +0900

    프로젝션(SELECT)

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,11 +1,18 @@
 package jpql;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
+import jpql.domain.Address;
 import jpql.domain.Member;
+import jpql.domain.Order;
+import jpql.domain.Team;
+import jpql.dto.MemberDTO;
 
 public class JpaMain {
 
@@ -23,12 +30,43 @@ public class JpaMain {
 			member.setUsername("member1");
 			member.setAge(10);
 			em.persist(member);
-			
-			Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
-				.setParameter("username", member.getUsername())
-				.getSingleResult();
 
-			System.out.println("result.getUsername() = " + result.getUsername());
+			em.flush();
+			em.clear();
+
+			// 엔티티 프로젝션
+			/*
+				JOIN 문은 성능 튜닝할 여지가 높으므로 JPQL문에서도 명시적으로 JOIN을 드러내는 것이 좋다.
+			 */
+			List<Team> result1 = em.createQuery("select m.team from Member m", Team.class) // 지양
+				.getResultList();
+
+			List<Team> result2 = em.createQuery("select t from Member m join m.team t", Team.class) // 지향
+				.getResultList();
+
+			// 임베디드 타입 프로젝션
+			List<Address> addresses = em.createQuery("select o.address from Order o", Address.class)
+				.getResultList();
+
+			// 스칼라 타입 프로젝션
+			List<String> result4 = em.createQuery("select distinct m.username from Member m", String.class)
+				.getResultList();
+
+			List result5 = em.createQuery("select distinct m.username, m.age from Member m")
+				.getResultList();
+
+			/*
+				NEW 명령어 - 단순 값을 DTO로 바로 조회
+			 */
+			List<MemberDTO> result6 = em.createQuery(
+				"select new jpql.dto.MemberDTO(m.username, m.age) "
+					+ "from Member m", MemberDTO.class
+				)
+				.getResultList();
+
+			MemberDTO memberDTO = result6.get(0);
+			System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
+
 
 			tx.commit();
 
---
+++ src/main/java/jpql/dto/MemberDTO.java
@@ -0,0 +1,20 @@
+package jpql.dto;
+
+public class MemberDTO {
+
+	private String username;
+	private int age;
+
+	public MemberDTO(String username, int age) {
+		this.username = username;
+		this.age = age;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public int getAge() {
+		return age;
+	}
+}
