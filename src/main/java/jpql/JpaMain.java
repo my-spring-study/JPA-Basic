ec57c3f5c1f070f57b1c0908df39298aabac3be5
Seonho Kim <sunhowold@naver.com>
2023-02-15 22:59:54 +0900

    페이징 API

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -26,47 +26,24 @@ public class JpaMain {
 
 		try {
 
-			Member member = new Member();
-			member.setUsername("member1");
-			member.setAge(10);
-			em.persist(member);
+			for (int i = 0; i < 100; i++) {
+				Member member = new Member();
+				member.setUsername("member" + i);
+				member.setAge(i);
+				em.persist(member);
+			}
 
 			em.flush();
 			em.clear();
 
-			// 엔티티 프로젝션
-			/*
-				JOIN 문은 성능 튜닝할 여지가 높으므로 JPQL문에서도 명시적으로 JOIN을 드러내는 것이 좋다.
-			 */
-			List<Team> result1 = em.createQuery("select m.team from Member m", Team.class) // 지양
+			List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+				.setFirstResult(0) // 조회 시작 위치 (0부터 시작)
+				.setMaxResults(10) // 조회할 데이터 수
 				.getResultList();
 
-			List<Team> result2 = em.createQuery("select t from Member m join m.team t", Team.class) // 지향
-				.getResultList();
-
-			// 임베디드 타입 프로젝션
-			List<Address> addresses = em.createQuery("select o.address from Order o", Address.class)
-				.getResultList();
-
-			// 스칼라 타입 프로젝션
-			List<String> result4 = em.createQuery("select distinct m.username from Member m", String.class)
-				.getResultList();
-
-			List result5 = em.createQuery("select distinct m.username, m.age from Member m")
-				.getResultList();
-
-			/*
-				NEW 명령어 - 단순 값을 DTO로 바로 조회
-			 */
-			List<MemberDTO> result6 = em.createQuery(
-				"select new jpql.dto.MemberDTO(m.username, m.age) "
-					+ "from Member m", MemberDTO.class
-				)
-				.getResultList();
-
-			MemberDTO memberDTO = result6.get(0);
-			System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
-
+			for (Member member : result) {
+				System.out.println("member = " + member);
+			}
 
 			tx.commit();
 
